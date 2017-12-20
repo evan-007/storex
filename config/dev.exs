c4f9e31d5bc58206c65823b63a37eb713d2e7316
Evan Lloyd <evan.u.lloyd@gmail.com>
2017-12-20 15:56:59 -0500

    Update db configs

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :storex, Storex.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "storex_dev",
   hostname: "localhost",
   pool_size: 10
