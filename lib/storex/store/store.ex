ca8a74346d142d2887aba2146d7f0d06e6e6de95
Evan Lloyd <evan.u.lloyd@gmail.com>
2017-12-21 08:29:53 -0500

    Move books to db

--- lib/storex/store/store.ex
+++ lib/storex/store/store.ex
@@ -1,38 +1,12 @@
 defmodule Storex.Store do
+  alias Storex.Repo
+  alias Storex.Store.Book
+
   def list_books() do
-    [
-      %{
-        id: "1",
-        title: "My first book",
-        description: "my first story",
-        price: "15.90",
-        image_url: "http://www.phoenixforrailsdevelopers.com/books/1.png",
-      },
-      %{
-        id: "2",
-        title: "My second book",
-        description: "my second story",
-        price: "15.90",
-        image_url: "http://www.phoenixforrailsdevelopers.com/books/2.png",
-      },
-      %{
-        id: "3",
-        title: "My third book",
-        description: "my third story",
-        price: "15.90",
-        image_url: "http://www.phoenixforrailsdevelopers.com/books/3.png",
-      },
-      %{
-        id: "4",
-        title: "My forth book",
-        description: "my forth story",
-        price: "15.90",
-        image_url: "http://www.phoenixforrailsdevelopers.com/books/4.png",
-      },
-    ]
+    Repo.all(Book)
   end
 
   def get_book(id) do
-    Enum.find(list_books(), fn(book) -> book.id == id end)
+    Repo.get(Book, id)
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,34 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+books = [
+  %{
+    title: "My first book",
+    description: "my first story",
+    price: "15.90",
+    image_url: "http://www.phoenixforrailsdevelopers.com/books/1.png",
+  },
+  %{
+    title: "My second book",
+    description: "my second story",
+    price: "15.90",
+    image_url: "http://www.phoenixforrailsdevelopers.com/books/2.png",
+  },
+  %{
+    title: "My third book",
+    description: "my third story",
+    price: "15.90",
+    image_url: "http://www.phoenixforrailsdevelopers.com/books/3.png",
+  },
+  %{
+    title: "My forth book",
+    description: "my forth story",
+    price: "15.90",
+    image_url: "http://www.phoenixforrailsdevelopers.com/books/4.png",
+  },
+]
+
+Enum.each(books, fn(attrs) ->
+  changeset = Storex.Store.Book.changeset(%Storex.Store.Book{}, attrs)
+  Storex.Repo.insert!(changeset)
+end)
