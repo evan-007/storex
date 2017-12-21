dc81caf2f580143b21f2697159cc51bc02167df6
Evan Lloyd <evan.u.lloyd@gmail.com>
2017-12-21 13:26:24 -0500

    Add plug for total price

--- lib/storex_web/views/layout_view.ex
+++ lib/storex_web/views/layout_view.ex
@@ -4,4 +4,8 @@ defmodule StorexWeb.LayoutView do
   def items_count(conn) do
     StorexWeb.Plugs.ItemsCount.get(conn)
   end
+
+  def items_total_price(conn) do
+    StorexWeb.Plugs.ItemsTotalPrice.get(conn)
+  end
 end
